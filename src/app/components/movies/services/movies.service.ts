ebc240687bdc677004ba5abb51126f0123f3588c
Jorge <jorgeMGdev@gmail.com>
2022-05-17 20:17:35 -0700

    fix on sorting algorithm on moviesService

--- src/app/components/movies/services/movies.service.ts
+++ src/app/components/movies/services/movies.service.ts
@@ -25,24 +25,24 @@ export interface Film {
 export class MoviesService {
   constructor(private readonly http: HttpClient) {}
 
-	getAllMovies(optionSelection: string, orderSelection: string): Observable<Film[]> {
+  getAllMovies(
+    optionSelection: string,
+    orderSelection: string
+  ): Observable<Film[]> {
     return this.http.get<any>(environment.api + 'films').pipe(
       pluck('results'),
       map(results =>
-					results.sort().sort((a: Film, b: Film) => {
-						if(optionSelection === '0' && orderSelection === '0'){
-							return a.episode_id - b.episode_id;
-						}	
-						else if(optionSelection === '0' && orderSelection === '1'){
-							return b.episode_id - a.episode_id;
-						}
-						else if(optionSelection === '1' && orderSelection === '0'){
-							return a.release_date.localeCompare(b.release_date) || b.episode_id - a.episode_id;
-						}
-						else {
-							return b.release_date.localeCompare(a.release_date) || b.episode_id - a.episode_id;
-						}
-					})
+        results.sort().sort((a: Film, b: Film) => {
+          if (optionSelection === '0' && orderSelection === '0') {
+            return a.episode_id - b.episode_id;
+          } else if (optionSelection === '0' && orderSelection === '1') {
+            return b.episode_id - a.episode_id;
+          } else if (optionSelection === '1' && orderSelection === '0') {
+            return a.release_date.localeCompare(b.release_date);
+          } else {
+            return b.release_date.localeCompare(a.release_date);
+          }
+        })
       )
     );
   }
